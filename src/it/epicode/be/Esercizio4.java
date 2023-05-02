061a4608daedf117797a365360c514837c9ecbf9
Nestor162 <nestorcicardini@gmail.com>
2023-05-02 17:22:45 +0200

    Perimetro triangolo

--- src/it/epicode/be/Esercizio4.java
+++ src/it/epicode/be/Esercizio4.java
@@ -1,4 +1,5 @@
 package it.epicode.be;
+import java.lang.Math; 
 
 public class Esercizio4 {
 
@@ -6,8 +7,11 @@ public class Esercizio4 {
 		System.out.println(perimetroRettangolo(5, 10));
 		
 		System.out.println(pariDispari(8));
+		
+		System.out.println(areaTriangolo(5, 7, 6));
 
 	}
+	
 	
 	public static int perimetroRettangolo(int l1 , int l2) {
 		int perimetro = (l1+l2)*2;
@@ -20,6 +24,19 @@ public class Esercizio4 {
 		} else {
 			return 1;
 		}
+		
+	}
+	
+	public static double areaTriangolo(float l1, float l2, float l3) {
+		float perimetro = l1 + l2 + l3;
+		float semiperimetro = perimetro/2;
+		
+		float x = semiperimetro - l1;
+		float y = semiperimetro - l2;
+		float z = semiperimetro - l3;
+		
+		double risultato = Math.sqrt(semiperimetro * x * y * z);
+		return risultato;
 	}
 
 }
