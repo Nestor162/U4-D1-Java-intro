5c69b1ac1be32d245b7f6225ad6cabcf78a82853
Nestor162 <nestorcicardini@gmail.com>
2023-05-02 17:09:40 +0200

    Pari dispari

--- src/it/epicode/be/Esercizio4.java
+++ src/it/epicode/be/Esercizio4.java
@@ -4,12 +4,22 @@ public class Esercizio4 {
 
 	public static void main(String[] args) {
 		System.out.println(perimetroRettangolo(5, 10));
+		
+		System.out.println(pariDispari(8));
 
 	}
 	
 	public static int perimetroRettangolo(int l1 , int l2) {
 		int perimetro = (l1+l2)*2;
 		return perimetro;
+	}
+	
+	public static int pariDispari(int n) {
+		if (n%2==0) {
+			return 0;
+		} else {
+			return 1;
+		}
 	}
 
 }
