2dc3f8bac8f55826df8fdc3b6d4f01310c12e711
Nestor162 <nestorcicardini@gmail.com>
2023-05-02 17:00:01 +0200

    Calcolo perimetro

---
+++ src/it/epicode/be/Esercizio4.java
@@ -0,0 +1,15 @@
+package it.epicode.be;
+
+public class Esercizio4 {
+
+	public static void main(String[] args) {
+		System.out.println(perimetroRettangolo(5, 10));
+
+	}
+	
+	public static int perimetroRettangolo(int l1 , int l2) {
+		int perimetro = (l1+l2)*2;
+		return perimetro;
+	}
+
+}
