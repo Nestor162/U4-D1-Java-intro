79104a480faa806e1b4844f965837f4578fd093e
Nestor162 <nestorcicardini@gmail.com>
2023-05-02 16:45:48 +0200

    Esercizio 3

---
+++ src/it/epicode/be/Exercise3.java
@@ -0,0 +1,17 @@
+package it.epicode.be;
+import java.util.Scanner;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter your name");
+		Scanner in = new Scanner(System.in);
+		String name = in.nextLine();
+		System.out.println("Enter your surname");
+		String surname = in.nextLine();
+		System.out.println("Enter your city's name");
+		String city = in.nextLine();
+
+	}
+
+}
