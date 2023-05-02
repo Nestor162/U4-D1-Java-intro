37679a4da33756a97d2e01ec324161e7e8f34f2e
Nestor162 <nestorcicardini@gmail.com>
2023-05-02 16:56:33 +0200

    Completato esercizio 3

--- src/it/epicode/be/Exercise3.java
+++ src/it/epicode/be/Exercise3.java
@@ -1,16 +1,33 @@
 package it.epicode.be;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Exercise3 {
 
 	public static void main(String[] args) {
+		
+		String[] strArray = new String[3];
+		
 		System.out.println("Enter your name");
 		Scanner in = new Scanner(System.in);
 		String name = in.nextLine();
+		strArray[0] = name;
+		
 		System.out.println("Enter your surname");
 		String surname = in.nextLine();
+		strArray[1] = surname;
+		
 		System.out.println("Enter your city's name");
 		String city = in.nextLine();
+		strArray[2] = city;
+		
+		
+		in.close();
+		System.out.println("Ordine di inserimento: " + Arrays.toString(strArray));
+		
+		Collections.reverse(Arrays.asList(strArray));
+		System.out.println("Ordine inverso: " + Arrays.toString(strArray));
 
 	}
 
